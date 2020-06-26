9e6b52fa380f209ad514be36cead058c502e0614
bkj281 <66874987+bkj281@users.noreply.github.com>
2020-06-26 17:58:15 +0530

    Update script.js

--- m4/script.js
+++ m4/script.js
@@ -1,7 +1,7 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 var i;
 function print() {
-    var i, a;
+    var a;
     for (i = 0; i < names.length; i++) {
         a = names[i].slice(0, 1);
         console.log(a);
