c5efb7552fd72d87a993d80b58806ec8fed2aef5
bkj281 <66874987+bkj281@users.noreply.github.com>
2020-07-13 19:45:54 +0530

    Delete SpeakGoodBye.js

--- m4/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-var byeSpeaker = {};
-
-( function (window) {
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function (name) {
-        console.log(speakWord + " " + name);
-    }
-})(window);
